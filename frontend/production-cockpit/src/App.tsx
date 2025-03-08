78e9a54cc617783e871a31ee66a686cfff85210b
Chema <jmpujades.dev@proton.me>
2025-03-08 12:16:54 +0100

    Add permanent Drawer component with a test ListItemButton to App

--- frontend/production-cockpit/src/App.tsx
+++ frontend/production-cockpit/src/App.tsx
@@ -1,9 +1,13 @@
+import { Drawer, ListItemButton } from "@mui/material";
 import "./App.css";
 import { MachineForm } from "./components/MachineForm";
 
 function App() {
   return (
     <>
+      <Drawer variant="permanent">
+        <ListItemButton>Test</ListItemButton>
+      </Drawer>
       <MachineForm />
     </>
   );
